7fc7b805059cac7279eb8813fbd0ae0d99963d7c
xumenger <xumenger@126.com>
2018-05-30 13:09:36 +0800

    add test

--- ruby/demo.rb
+++ ruby/demo.rb
@@ -1,5 +1,9 @@
+# ruby demo.rb 运行程序
+
+require "digest"
 
 class Block
+
     attr_reader :timestamp
     attr_reader :data
     attr_reader :previous_hash
@@ -9,7 +13,7 @@ class Block
         @timestamp     = Time.now
         @data          = data
         @previous_hash = previous_hash
-        @hash = calc_hash
+        @hash          = calc_hash
     end
 
     # 创世区块
@@ -22,6 +26,7 @@ class Block
     end
 
 private
+
     def calc_hash
         sha = Digest::SHA256.new
         sha.update(@timestamp.to_s + @previous_hash + @data)
@@ -31,4 +36,13 @@ private
 end
 
 
+# 测试
+b0 = Block.first("Genesis")
+b1 = Block.next( b0, "Transaction Data..." )
+b2 = Block.next( b1, "Transaction Data......" )
+b3 = Block.next( b2, "More Transaction Data..." )
+
+blockchain = [b0, b1, b2, b3]
+
+pp blockchain
 
