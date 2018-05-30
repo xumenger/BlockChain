cc5348b4a373321b3087191e1a13e0e7f78180bb
xumenger <xumenger@126.com>
2018-05-30 12:43:20 +0800

    get next block

--- python/demo.py
+++ python/demo.py
@@ -26,3 +26,15 @@ def create_genesis_block():
     return Block(0, date.datetime.now(), "Genesis Block", "0")
 
 
+"""生成区块链中的后续区块
+"""
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    # 这里写死data是一个字符串
+    # 其实在区块链中，data可以是任意你想要存储的信息
+    this_data = "Hey! I'm block " + str(this_index)
+    this_previous_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_previous_hash)
+
+
